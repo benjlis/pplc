75a2a9e8b8349230888130932db408c385155ce2
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-21 13:30:54 -0400

    I/O specified on command line

--- linkcheck.py
+++ linkcheck.py
@@ -1,4 +1,5 @@
 import requests
+import argparse
 
 def check_url(url):
     try:
@@ -11,11 +12,16 @@ def check_urls_from_file(input_filename, output_filename):
     with open(input_filename, 'r') as input_file, open(output_filename, 'w') as output_file:
         for line in input_file:
             url = line.strip()  # Remove newline characters and leading/trailing whitespaces
+            print(url)
             result = check_url(url)
             status = 'Y' if result else 'N'
-            output_file.write(f"{url} {status}\n")
+            output_file.write(f"{url}, {status}\n")
 
-# Example usage:
-input_filename = 'test-urls.txt'
-output_filename = 'test-urls.out'
-check_urls_from_file(input_filename, output_filename)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Check the validity of URLs from a file.")
+    parser.add_argument("input_file", help="The name of the input file containing URLs, one per line.")
+    parser.add_argument("output_file", help="The name of the output file to store the results.")
+    args = parser.parse_args()
+
+    check_urls_from_file(args.input_file, args.output_file)
